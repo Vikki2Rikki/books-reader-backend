76e9ceb9bbf37450470c2fd0fa8ff6bdbb1f7555
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-09-19 16:39:29 +0300

    up

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
+const authRouter = require("./routes/api/auth");
 const booksRouter = require("./routes/api/books");
-const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
@@ -13,8 +14,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRouter);
 app.use("/api/books", booksRouter);
-app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
--- routes/api/books.js
+++ routes/api/books.js
@@ -1,89 +1,28 @@
 const express = require("express");
-const Joi = require("joi");
 
-const books = require("../../models/books");
+const ctrl = require("../../controllers/books");
 
-const { HttpError } = require("../../helpers");
+const { validateBody, isValidId } = require("../../middlewares");
+
+const { schemas } = require("../../models/book");
 
 const router = express.Router();
 
-const addSchema = Joi.object({
-  title: Joi.string().required(),
-  author: Joi.string().required(),
-});
+router.get("/", ctrl.getAll);
 
-router.get("/", async (req, res) => {
-  try {
-    const result = await books.getAll();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", isValidId, ctrl.getById);
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await books.getById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-      // const error = new Error("Not found");
-      // error.status = 404;
-      // throw error;
-      // return res.status(404).json({
-      //     message: "Not found"
-      // })
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await books.add(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { id } = req.params;
-    const result = await books.updateById(id, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await books.deleteById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    // res.status(204).send()
-    res.json({
-      message: "Delete success",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:id", isValidId, ctrl.deleteById);
 
 module.exports = router;
